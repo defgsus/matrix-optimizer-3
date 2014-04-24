f8a3c466f48734c8ef8dbadc8ff32ba30479b19e
def.gsus- <stefan.berke@modular-audio-graphics.com>
2014-04-24 19:50:53 +0200

    added linear and smoothstep functions

--- src/math/interpol.h
+++ src/math/interpol.h
@@ -23,6 +23,35 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 namespace MO {
 
+/** Put the value of @p x between [0,1]. <br/>
+    a linear fade between 0 and 1 happens for @p x in the range [@p edge0,@p egde1]. <br/>
+    edge0 and egde1 must not be equal! */
+template <typename F>
+inline F linearstep(F edge0, F edge1, F x)
+{
+    return std::max((F)0, std::min((F)1, (x-edge0) / (edge1-edge0) ));
+}
+
+/** Put the value of @p x between [0,1]. <br/>
+    a smooth fade between 0 and 1 happens for @p x in the range [@p edge0,@p egde1]. <br/>
+    edge0 and egde1 must not be equal! */
+template <typename F>
+inline F smoothstep(F edge0, F edge1, F x)
+{
+    x = linearstep(edge0, edge1, x);
+    return x * x * ((F)3 - (F)2 * x);
+}
+
+/** Put the value of @p x between [0,1]. <br/>
+    a smooth fade between 0 and 1 happens for @p x in the range [@p edge0,@p egde1]. <br/>
+    edge0 and egde1 must not be equal! */
+template <typename F>
+inline F smootherstep(F edge0, F edge1, F x)
+{
+    x = linearstep(edge0, edge1, x);
+    return x * x * x * (x * (x * (F)6 - (F)15) + (F)10);
+}
+
 /** 6-point interpolation. <br>
     from somewhere in musicdsp.org <br>
 
