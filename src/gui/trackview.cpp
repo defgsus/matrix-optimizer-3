9d40aa6923d1eebbd2f42e630148143bfe404993
def.gsus- <stefan.berke@modular-audio-graphics.com>
2014-07-04 12:28:27 +0200

    mhh, probably can't use QGraphicsView for the Sequencer, it does too much viewport adjustment by itself

--- src/gui/trackview.cpp
+++ src/gui/trackview.cpp
@@ -8,6 +8,8 @@
     <p>created 7/3/2014</p>
 */
 
+#include <QDebug>
+
 #include <QGraphicsScene>
 #include <QGraphicsRectItem>
 #include <QGraphicsTextItem>
@@ -22,6 +24,10 @@ TrackView::TrackView(QWidget *parent) :
     //QWidget(parent)
 {
     setMinimumSize(320,240);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setTransformationAnchor(QGraphicsView::NoAnchor);
+    setAlignment(0);
+
     /*
     QPalette p(palette());
     p.setColor(QPalette::Background, QColor(50,50,50));
@@ -30,7 +36,6 @@ TrackView::TrackView(QWidget *parent) :
     */
     scene_ = new QGraphicsScene(this);
     scene_->setBackgroundBrush(QBrush(QColor(150,150,150)));
-    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
     setScene(scene_);
 
@@ -39,13 +44,19 @@ TrackView::TrackView(QWidget *parent) :
 }
 
 
-
+QGraphicsRectItem * seq;
 
 void TrackView::createItems_()
 {
-    auto r = scene_->addRect(10,10,50000,20);
+    /*
+    auto r = scene_->addRect(10,10,50000,20,QPen(), QBrush(QColor(100,120,100)));
     auto t = scene_->addText("blabla");
     t->setParentItem(r);
+    */
+    seq = scene_->addRect(50,40,50,20, QPen(), QBrush(QColor(100,120,100)));
+
+    scene_->addLine(0,0, 0,300);
+    scene_->addLine(100,0, 100,300);
 /*
     QPalette p(palette());
     p.setColor(QPalette::Background, QColor(80,120,80));
@@ -72,7 +83,25 @@ void TrackView::setViewSpace(const UTIL::ViewSpace & s)
 {
     space_ = s;
 
+    QRectF r(seq->rect());
+    r.setLeft(space_.mapXFrom(0) * width());
+    r.setRight(space_.mapXFrom(1) * width());
+    seq->setRect(r);
 
+    //QTransform t;
+    //t.scale(1.0 / space_.scaleX(), 1);
+    //t.translate(-space_.x() * width(), 0);
+    //setTransform(t);
+    /*
+    QRectF r = sceneRect();
+    qDebug() << r;
+    r.moveLeft(r.left() + space_.x());//space_.x());
+    qDebug() << r;
+    setSceneRect(r);
+    */
+    //setSceneRect(QRectF(space_.x() * width(), sceneRect().top(),
+    //            width() / space_.scaleX(), sceneRect().height()));
+    //qDebug() << sceneRect();
 }
 
 } // namespace GUI
